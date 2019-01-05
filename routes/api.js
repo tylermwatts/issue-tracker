0958b0e20f7fc00dbbd6f10513087595411f0f96
Glitch (fearless-marble) <none>
2019-01-05 02:47:58 +0000

    💡🍼 Checkpoint ./routes/api.js:2521121/634

--- routes/api.js
+++ routes/api.js
@@ -32,8 +32,17 @@ module.exports = function (app) {
           var project = req.params.project;
           db.collection("issues").insertOne({
             issue_title: req.body.issue_title,
-            
-          }
+            issue_text: req.body.issue_text,
+            created_by: req.body.created_by,
+            assigned_to: req.body.assigned_to,
+            status_text: req.body.status_text
+          }, (err, issue) => {
+            if (err){
+              return res.json(err)
+            }
+            console.log(issue);
+            res.json(issue);
+          })
         })
     
         .put(function (req, res){
