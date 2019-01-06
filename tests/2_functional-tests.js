4cb63ed3924f245ee347ad110072eebab5e74167
Glitch (fearless-marble) <none>
2019-01-06 22:44:41 +0000

    💋🏗 Checkpoint ./tests/2_functional-tests.js:2521121/724

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -185,13 +185,10 @@ suite('Functional Tests', function() {
       
       test('Valid _id', function(done) {
           chai.request(server)
-          .get('/api/issues/test')
-          .query({})
-          .end((err,res)=>{
-            console.log(res.locals)
+          .delete('/api/issues/test', (req,res)=>{
+            req.firstIssue = res.body[0]._id;
+            console.log(req.firstIssue)
           })
-        chai.request(server)
-          .delete('/api/issues/test')
           .query({_id: ''})
           .end((err,res)=>{
             assert.equal(res.status, 200);
